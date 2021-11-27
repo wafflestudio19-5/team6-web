17b1e14562984aee14a1a00bd41018322d5a53c1
s-yeoyul <springyy1011@snu.ac.kr>
2021-11-27 15:19:25 +0900

    feat: Implement product image carousel

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -3,9 +3,13 @@ import { useEffect, useState } from "react";
 import styles from "./Article.module.scss";
 import dummyData from "./DummyData";
 import leftArrowIcon from "../../icons/leftArrow.png";
-import homeIcon from "../../icons/home.png"
-import shareIcon from "../../icons/share.png"
-import moreIcon from "../../icons/more.png"
+import homeIcon from "../../icons/home.png";
+import shareIcon from "../../icons/share.png";
+import moreIcon from "../../icons/more.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 type userData = {
   id: number;
   name: string;
@@ -23,10 +27,32 @@ type userData = {
   hit: number;
 };
 // 판매글 API랑 메인 페이지가 완성되지 않아서 더미데이터로 구현했습니다.
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 200,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+}; // https://sirong.tistory.com/38
+
 const Article = () => {
   const { id } = useParams() as { id: string };
   const [user, setUser] = useState<userData | null>(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setUser(dummyData.filter((data) => data.id === parseInt(id))[0]);
+  }, [id]);
+
+  const carouselImg = user?.product_img.map((image) => {
+    return (
+      <div>
+        <img className={styles.carouselImg} src={image} alt={"상품 이미지"} />
+      </div>
+    );
+  });
 
   const onClickArrow = () => {
     navigate("/main");
@@ -41,18 +67,37 @@ const Article = () => {
     console.log("Report this user");
   };
 
-  useEffect(() => {
-    setUser(dummyData.filter((data) => data.id === parseInt(id))[0]);
-  }, [id]);
-
   return (
     <>
       <div className={styles.articleWrapper}>
         <div className={styles.header}>
-          <img className={styles.backButton} src={leftArrowIcon} alt={"뒤로 가기"} onClick={onClickArrow} />
-          <img className={styles.homeButton} src={homeIcon} alt={"뒤로 가기"} onClick={onClickHome} />
-          <img className={styles.shareButton} src={shareIcon} alt={"뒤로 가기"} onClick={onClickShare} />
-          <img className={styles.reportButton} src={moreIcon} alt={"뒤로 가기"} onClick={onClickReport} />
+          <img
+            className={styles.backButton}
+            src={leftArrowIcon}
+            alt={"뒤로 가기"}
+            onClick={onClickArrow}
+          />
+          <img
+            className={styles.homeButton}
+            src={homeIcon}
+            alt={"뒤로 가기"}
+            onClick={onClickHome}
+          />
+          <img
+            className={styles.shareButton}
+            src={shareIcon}
+            alt={"뒤로 가기"}
+            onClick={onClickShare}
+          />
+          <img
+            className={styles.reportButton}
+            src={moreIcon}
+            alt={"뒤로 가기"}
+            onClick={onClickReport}
+          />
+        </div>
+        <div className={styles.carousel}>
+          <Slider {...settings}>{carouselImg}</Slider>
         </div>
       </div>
     </>
