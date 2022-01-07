0d890cd25ae5ef8cf99f437ea96956f3587f4787
JiyeonP <q0o0p@snu.ac.kr>
2022-01-08 06:40:58 +0900

    feat:setting page removed phone number

--- src/components/Main/Settings/Account.tsx
+++ src/components/Main/Settings/Account.tsx
@@ -1,8 +1,19 @@
 import styles from "./Settings.module.scss";
 import backArrow from "../../../icons/leftArrow.png";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { requester } from "../../../apis/requester";
+import { userType } from "../../../type/types";
 
 const Account = (props: { setAccount: Dispatch<SetStateAction<boolean>> }) => {
+  const [user, setUser] = useState<userType | null>(null);
+  useEffect(() => {
+    requester.get("/users/me/").then((res) => {
+      setUser(res.data);
+    });
+  });
+  if (!user) {
+    return <div className={styles.wrapper} />;
+  }
   return (
     <div className={styles.wrapper}>
       <div className={styles.subHeader}>
@@ -17,16 +28,15 @@ const Account = (props: { setAccount: Dispatch<SetStateAction<boolean>> }) => {
       <div className={styles["section-wrapper"]}>
         <div className={styles.section}>
           <div className={styles.title}>계정 정보</div>
-          <div className={styles.button}>이메일</div>
           <div className={styles.button}>
-            휴대폰 번호
-            <div className={styles.contents}>010-XXXX-XXXX</div>
+            이메일
+            <div className={styles.contents}>{user.email}</div>
           </div>
         </div>
-        <div className={styles.section}>
-          <div className={styles.title}>기타</div>
-          <div className={styles.button}>서비스 이용약관</div>
-        </div>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.title}>기타</div>
+        <div className={styles.button}>서비스 이용약관</div>
       </div>
     </div>
   );
