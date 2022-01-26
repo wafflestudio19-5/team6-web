e5472af6de9eb4a8e505cefe0a42b262637316cf
JiyeonP <q0o0p@snu.ac.kr>
2022-01-27 00:45:46 +0900

    before rebase

--- src/type/dto/for-api/patch-product-request.dto.ts
+++ src/type/dto/for-api/patch-product-request.dto.ts
@@ -3,12 +3,12 @@ import { ForAge } from "../../enum/for-age";
 import { RangeOfLocation } from "../../enum/range-of-location";
 
 export type PatchProductRequestDto = {
-  category: Category;
-  content: string;
-  for_age: ForAge;
-  image_url: string[];
-  negotiable: boolean;
-  price: number;
-  range_of_location: RangeOfLocation;
-  title: string;
+  category?: Category;
+  content?: string;
+  for_age?: ForAge;
+  image_url?: string[];
+  negotiable?: boolean;
+  price?: number;
+  range_of_location?: RangeOfLocation;
+  title?: string;
 };
--- src/type/enum/status-action.ts
+++ src/type/enum/status-action.ts
@@ -1,1 +1,8 @@
-export enum StatusAction {}
+export enum StatusAction {
+  RESERVED = "reserved",
+  SOLD_OUT = "sold out",
+  FOR_SALE = "for sale",
+  HIDE = "hide",
+  SHOW = "show",
+  BUMP = "bump",
+}
